78e2ec8082b9fa209c9f6d757220863e0d1e1a1e
Dragan Cekic <dragancekic718@gmail.com>
2025-12-11 17:25:09 +0100

    Add project files.

---
+++ Zadatak_1/Automobil.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1
+{
+    internal class Automobil
+    {
+         
+
+        public string Marka {  get; set; }
+     public string Model { get; set; }
+     public int Godiste { get; set; }
+     public int MaxBrzina { get; set; }
+     public int TrenutnaBrzina { get; set; }
+        
+     
+     public string Boja { get; set; }
+     
+     public Automobil(string marka,string model,int godiste, int maxBrzina,int trenutnaBrzina, string boja)
+        {
+            this.Marka = marka;
+            this.Model = model;
+            this.Godiste = godiste;
+            this.Boja = boja;
+            this.MaxBrzina = maxBrzina;
+            this.TrenutnaBrzina = trenutnaBrzina;
+           
+        }
+        
+        public  void IspisiAtribute()
+        {
+            Console.WriteLine($"Marka: {Marka}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Godiste: {Godiste}");
+            Console.WriteLine($"Boja: {Boja}");
+            Console.WriteLine($"Maksimalna Brzina: {MaxBrzina}");
+            Console.WriteLine($"Trenutna Brzina: {TrenutnaBrzina}");
+           
+            Console.WriteLine("-------------------------");
+        }
+
+        public void Farbaj(string boja)
+        {
+            this.Boja = boja;
+        }
+        
+        public int Ubrzaj()
+        {
+            this.TrenutnaBrzina += 5;
+            if (TrenutnaBrzina >= MaxBrzina)
+                TrenutnaBrzina = MaxBrzina;
+            return this.TrenutnaBrzina;
+        }
+        public int Uspori()
+        {
+          TrenutnaBrzina -= 5;
+            if (TrenutnaBrzina < 0)
+                TrenutnaBrzina = 0;
+            return TrenutnaBrzina;
+        }
+     
+
+    }
+}
---
+++ Zadatak_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Automobil a1 = new Automobil("Seat","Leon",2012,190,150,"Bela");
+            Automobil a2 = new Automobil("Bmw", "M5", 2020, 270, 260, "Crna");
+            a1.IspisiAtribute();
+
+
+            a2.Ubrzaj();
+            a2.Ubrzaj();
+            a2.IspisiAtribute();
+            a2.Uspori();
+            a2.IspisiAtribute();
+
+
+
+        }
+       
+    }
+}
